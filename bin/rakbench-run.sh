ad58122840809d18fda95299eff659c0b51c39ff
pmichaud <pmichaud@pobox.com>
2011-05-04 08:45:40 -0500

    Add some machine statistics (memory, OS) to the output of rakbench-run.sh .

--- bin/rakbench-run.sh
+++ bin/rakbench-run.sh
@@ -5,9 +5,15 @@ mkdir -p log
 
 LOGFILE=$(date +log/rakbench-$HOSTNAME-$ARCH-%Y%m%d%H%M.log)
 
-(for t in bin/rakbench-??.sh
+(
+# output some statistics
+uname -a
+grep MemTotal /proc/meminfo
+# run the suite
+for t in bin/rakbench-??.sh
 do
   echo "== $t start =="
   sh $t
   echo "== $t end =="
-done) |& tee $LOGFILE
+done
+) |& tee $LOGFILE
