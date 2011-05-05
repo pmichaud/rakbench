54d5c6090355cf39550f554b3d0d907c6b54dde9
Patrick R. Michaud <pmichaud@pobox.com>
2011-05-05 12:48:28 -0500

    Add some date wrappers to script invocations.

--- bin/rakbench-run.sh
+++ bin/rakbench-run.sh
@@ -9,11 +9,14 @@ LOGFILE=$(date +log/$HOSTNAME-$ARCH-%Y%m%d%H%M.log)
 # output some statistics
 uname -a
 grep MemTotal /proc/meminfo
+date
 # run the suite
 for t in bin/rakbench-??.sh
 do
   echo "== $t start =="
+  date
   sh $t
+  date
   echo "== $t end =="
 done
 ) |& tee $LOGFILE
