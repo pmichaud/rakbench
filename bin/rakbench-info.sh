5835e7bc9ea8cd6ac7093668ae3602f3cae18165
pmichaud <pmichaud@pobox.com>
2011-05-09 16:49:59 -0500

    Script to automatically populate $build/RAKBENCH files.

---
+++ bin/rakbench-info.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for build in "$@"
+do
+  RAKBENCH = $build/RAKBENCH
+  [ -f $RAKBENCH ] && 
+    { echo "$RAKBENCH exists...skipping"; continue; }
+  echo "Creating $RAKBENCH..."
+  GC_TYPE=$($build/parrot_install/bin/parrot_config gc_type)
+  GC_TYPE=${GC_TYPE/*no such key*/unk}
+  echo "NICK=$(<$build/VERSION)/$(<$build/parrot/VERSION) (${GC_TYPE,,})" > $RAKBENCH
+  echo "# NOTE=Additional note(s) here" >> $RAKBENCH
+  cat $RAKBENCH
+done
