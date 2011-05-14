6b4ff8ba80221a76c5e7f4656fe8c375361cfbb9
pmichaud <pmichaud@pobox.com>
2011-05-14 01:31:36 -0500

    Fix some parrot_install/prefix issues, be sure to have bleeding parrot.

--- bin/rakbench-bleed.sh
+++ bin/rakbench-bleed.sh
@@ -4,20 +4,24 @@
   { echo "No rakudo-bleed directory...aborting"; exit 1; }
 
 cd rakudo-bleed
+PARROT_INSTALL_DIR=$(readlink -f parrot_install)
 [ -f RAKBENCH ] ||
   echo "NICK=rakudo-bleed (gms)" >RAKBENCH
 git pull
 git checkout master
-rm -rf parrot_install
+rm -rf $PARROT_INSTALL
 (
+  [ -d parrot ] || git clone git@github.com:parrot/parrot.git
   cd parrot &&
-    make realclean &&
-    perl Configure.pl --optimize --gc=gms --prefix=../parrot_install &&
+    git pull &&
+    git checkout master &&
+    make realclean;
+    perl Configure.pl --optimize --gc=gms --prefix=$PARROT_INSTALL &&
     make &&
     make install
 ) &&
   perl Configure.pl --makefile-timing --parrot-config=parrot_install/bin/parrot_config &&
-  make testable &&
+  make t/spec &&
   echo "rakudo-bleed complete"
 
 
